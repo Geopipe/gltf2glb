9799670aa47e4fcbd98e1416ef179494a0da1cfa
Christopher Mitchell <github@cemetech.net>
2017-05-28 16:33:10 -0400

    Updated b3dm to match AnalyticalGraphics/3d-tiles@e6caa00

--- b3dm.py
+++ b3dm.py
@@ -8,28 +8,40 @@
 
 import struct
 from batchtable import BatchTable
+from featuretable import FeatureTable
 
 B3DM_VERSION = 1
-B3DM_HEADER_LEN = 24
+B3DM_HEADER_LEN = 28
 
 class B3DM(BatchTable):
 	def __init__(self):
-		BatchTable.__init__(self)
+		self.batch_table = BatchTable()
+		self.feature_table = FeatureTable()
 		self.gltf_bin = bytearray()
 
-	def writeBinary(self, gltf_bin, num_features = 0):
-		self.finalize()
+	def writeBinary(self, gltf_bin, num_batch_features = 0, num_feature_features = 0):
+		self.batch_table.finalize()
+		self.feature_table.finalize()
 
 		# Generate the header
-		num_features = max(num_features, self.getNumFeatures())
-		output = self.writeHeader(gltf_bin, num_features)
+		num_batch_features =   max(num_batch_features,   self.batch_table.getNumFeatures())
+		num_feature_features = max(num_feature_features, self.feature_table.getNumFeatures())
+		output = self.writeHeader(gltf_bin, num_batch_features, num_feature_features)
+
+		# Add the feature table JSON to the output
+		feature_json = self.feature_table.getBatchJSON()
+		output.extend(feature_json)
+
+		# Add the batch table binary to the output
+		feature_bin  = self.feature_table.getBatchBin()
+		output.extend(feature_bin)
 
 		# Add the batch table JSON to the output
-		batch_json = self.getBatchJSON()
+		batch_json = self.batch_table.getBatchJSON()
 		output.extend(batch_json)
 
 		# Add the batch table binary to the output
-		batch_bin  = self.getBatchBin()
+		batch_bin  = self.batch_table.getBatchBin()
 		output.extend(batch_bin)
 
 		# Add the GLTF model body to the output
@@ -37,19 +49,25 @@ class B3DM(BatchTable):
 
 		return output
 
-	def writeHeader(self, gltf_bin, num_features):
-		batch_json = self.getBatchJSON()
-		batch_bin  = self.getBatchBin()
+	def writeHeader(self, gltf_bin, num_feature_features, num_batch_features):
+		len_feature_json = len(self.feature_table.getBatchJSON())
+		len_feature_bin  = len(self.feature_table.getBatchBin())
+		len_batch_json   = len(self.batch_table.getBatchJSON())
+		len_batch_bin    = len(self.batch_table.getBatchBin())
 
-		length = B3DM_HEADER_LEN + len(batch_json) + \
-		         len(batch_bin) + len(gltf_bin)
+		length = B3DM_HEADER_LEN + \
+		         len_feature_json + len_feature_bin + \
+		         len_batch_json   + len_batch_bin + \
+		         len(gltf_bin)
 	
 		output = bytearray()
 		output.extend("b3dm")
 		output.extend(struct.pack('<I', B3DM_VERSION))
 		output.extend(struct.pack('<I', length))
-		output.extend(struct.pack('<I', len(self.batch_json)))
-		output.extend(struct.pack('<I', len(self.batch_bin)))
+		output.extend(struct.pack('<I', len_feature_json))
+		output.extend(struct.pack('<I', len_feature_bin))
+		output.extend(struct.pack('<I', len_batch_json))
+		output.extend(struct.pack('<I', len_batch_bin))
 		output.extend(struct.pack('<I', num_features))
 	
 		# Sanity check
